c086ad584d76f35867466b6c25a0da19c1442622
vnimnualrat <vnimnualrat@gmail.com>
2016-02-27 17:41:33 -0800

    Completed TF

--- Crawler/src/main/java/index/Indexing.java
+++ Crawler/src/main/java/index/Indexing.java
@@ -3,11 +3,10 @@ package index;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
-import java.text.DecimalFormat;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Scanner;
-import java.util.TreeMap;
 
 import org.apache.tika.exception.TikaException;
 import org.apache.tika.metadata.Metadata;
@@ -20,73 +19,73 @@ import org.xml.sax.SAXException;
 
 public class Indexing {
 
-    private static HashMap<String, Integer> map = new HashMap<String, Integer>();
-    private static TreeMap<String, Integer> frequencies = new TreeMap<String, Integer>();
-    // Another table for term, list of string
-    // Another table for term, positions
-
-    public Indexing() {
-    };
+    private static HashMap<String, Double> termFrequency = new HashMap<String, Double>();
+    private static HashMap<String, ArrayList<Integer>> wordPosition = new HashMap<String, ArrayList<Integer>>();
+    // Term, URL
+    private static HashMap<String, ArrayList<String>> TermURL = new HashMap<String, ArrayList<String>>();
 
     public static void main(String[] args) throws IOException, SAXException, TikaException {
 
 	File file = new File("(15810)_1994_JR1_9064.html");
 	String content = extractHtml(file);
-
-	int numberOfTerms = wordCount(content);
-//	System.out.println(termFrequency(frequencies, "an", numberOfTerms));
-	 System.out.println(wordCount(content));
-//	 String a = "of";
-//	 System.out.println(isStopWord(a));
-	// System.out.println(content);
+	calculateTF(termFrequency, wordCount(content));
     }
 
     public static final String[] ENGLISH_STOP_WORDS = { "a", "an", "and", "are", "as", "at", "be", "but", "by", "for",
 	    "if", "in", "into", "is", "it", "no", "not", "of", "on", "or", "such", "that", "the", "their", "then",
 	    "there", "these", "they", "this", "to", "was", "will", "with" };
 
-    public static int wordCount(String text) {
+    public static double wordCount(String text) {
+
 	int numberOfTerms = 0;
+
 	Scanner in = new Scanner(text);
 	while (in.hasNext()) {
-	    numberOfTerms++;
-//	    System.out.println("in.next()"+in.next().toLowerCase());
+
 	    // Convert the word into lower case *unique*
 	    String word = convertToLowerCase(in.next());
-	    
-//	    System.out.println("Word: " + word + " |||| Is a stop word: " + isStopWord(word));
-	    
+
 	    // If it is NOT an empty space or a STOP WORD; continue
 	    if (word != "" && (isStopWord(word) == false)) {
-		// System.out.println("Word: " + "'"+word+"'" + " ---
-		// isStopWord: " + isStopWord(word));
-		Integer count = map.get(word);
+		numberOfTerms++;
+		Double count = termFrequency.get(word);
+
 		// If word isn't already in the map, initialize it at 1
 		if (count == null) {
-		    count = 1;
+		    count = 1.0;
 		} else { // If word is already in the map, increment
-		    count += 1;
+		    count += 1.0;
 		}
 
-		map.put(word, count);
-		frequencies.clear();
-		frequencies.putAll(map);
+		termFrequency.put(word, count);
 	    }
+
 	}
-	for (String key : map.keySet()) {
-	    System.out.println(key + " : " + map.get(key));
-	}
+
+	// printHashMap(termFrequency);
+	// printWordPosition(wordPosition);
+
 	return numberOfTerms;
     }
 
-    public static double termFrequency(TreeMap<String, Integer> map, String term, int numberofterms) {
-	DecimalFormat formatter = new DecimalFormat("#0.00000000");
-	double tf = 0;
-	tf = ((map.get(term).doubleValue()) / numberofterms);
-	System.out.println("map get term value: " + map.get(term));
-	System.out.println("number of terms: " + numberofterms);
-	System.out.println("tf: " + formatter.format(tf));
-	return tf;
+    public static void printHashMap(HashMap<String, Double> map) {
+	for (String key : map.keySet()) {
+	    System.out.println(key + " : " + map.get(key));
+	}
+    }
+
+    public static void calculateTF(HashMap<String, Double> map, double numberOfTerms) {
+
+	for (Map.Entry<String, Double> entry : map.entrySet()) {
+	    entry.setValue((entry.getValue() / numberOfTerms) - entry.getValue());
+	}
+    }
+
+    public static double inverseDocumentFrequency(HashMap<String, Integer> map) {
+	// number of doc = size of ocllection
+	//
+	return 0;
+
     }
 
     // Checks to see if the word is a stop word
@@ -113,7 +112,7 @@ public class Indexing {
 		lowerCasedWord = lowerCasedWord + c;
 	    }
 	}
-	
+
 	return lowerCasedWord.toLowerCase();
     }
 
