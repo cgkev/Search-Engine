999f99c15e599f0c21ed4336aeeee348bf3b2486
vnimnualrat <vnimnualrat@gmail.com>
2016-03-20 00:42:43 -0700

    Updated Search Model & Word Positioning.

--- Crawler/src/main/java/searching_features/Search.java
+++ Crawler/src/main/java/searching_features/Search.java
@@ -1,17 +1,32 @@
 package searching_features;
 
-public class Search implements Comparable<Search>{
+import java.util.ArrayList;
+
+public class Search implements Comparable<Search> {
 
 	String URL;
 	double linkAnalysis;
 	double tfidf;
 	double totalRanking;
-	
-	public Search(String URL, double linkAnalysis, double tfidf, double totalRanking){
+	ArrayList<Integer> position = new ArrayList<Integer>();
+	String body;
+
+	public Search(String URL, double linkAnalysis, double tfidf, double totalRanking, ArrayList<Integer> position,
+			String body) {
 		this.URL = URL;
 		this.linkAnalysis = linkAnalysis;
 		this.tfidf = tfidf;
 		this.totalRanking = totalRanking;
+		this.position = position;
+		this.body = body;
+	}
+
+	public String getBody() {
+		return body;
+	}
+
+	public void setBody(String body) {
+		this.body = body;
 	}
 
 	public String getURL() {
@@ -46,11 +61,17 @@ public class Search implements Comparable<Search>{
 		this.totalRanking = totalRanking;
 	}
 
+	public ArrayList<Integer> getPosition() {
+		return position;
+	}
+
+	public void setPosition(ArrayList<Integer> position) {
+		this.position = position;
+	}
+
 	@Override
 	public int compareTo(Search o) {
 		return Double.compare(totalRanking, o.getTotalRanking());
 	}
+
 }
-
-
-
--- Crawler/src/main/java/searching_features/SearchFeatures.java
+++ Crawler/src/main/java/searching_features/SearchFeatures.java
@@ -1,8 +1,18 @@
 package searching_features;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
+
+import org.apache.tika.exception.TikaException;
+import org.apache.tika.metadata.Metadata;
+import org.apache.tika.parser.ParseContext;
+import org.apache.tika.parser.html.HtmlParser;
+import org.apache.tika.sax.BodyContentHandler;
+import org.xml.sax.SAXException;
 
 import com.mongodb.BasicDBObject;
 import com.mongodb.DB;
@@ -18,8 +28,20 @@ public class SearchFeatures {
 	static DBCollection rankDB = database.getCollection("rank");
 	static DBCollection indexDB = database.getCollection("index");
 
+	public static String extractHtml(File fileName) throws IOException, SAXException, TikaException {
+		BodyContentHandler handler = new BodyContentHandler(10 * 1024 * 1024);
+		Metadata metadata = new Metadata();
+		FileInputStream inputstream = new FileInputStream(fileName);
+		ParseContext pcontext = new ParseContext();
+
+		HtmlParser htmlparser = new HtmlParser();
+		htmlparser.parse(inputstream, handler, metadata, pcontext);
+		
+		return handler.toString();
+	}
+
 	// parses "x and y" or "x or y" to computer readable
-	public static List<Search> boolDriver(String boolSearch) {
+	public static List<Search> boolDriver(String boolSearch) throws IOException, SAXException, TikaException {
 		String[] data = boolSearch.toLowerCase().split(" ");
 
 		if (data[1].equals("and")) {
@@ -35,7 +57,8 @@ public class SearchFeatures {
 
 	// isAnd = true = AND
 	// is And = false = OR
-	public static List<Search> boolSearch(String word1, String word2, boolean isAnd) {
+	@SuppressWarnings("unchecked")
+	public static List<Search> boolSearch(String word1, String word2, boolean isAnd) throws IOException, SAXException, TikaException {
 		List<Search> search = new ArrayList<Search>();
 
 		// and boolean
@@ -46,16 +69,22 @@ public class SearchFeatures {
 			List<Search> tempAll = new ArrayList<Search>();
 
 			DBCursor w1 = indexDB.find(new BasicDBObject("Term", word1));
+			ArrayList<Integer> position = new ArrayList<Integer>();
 
 			for (DBObject link : w1) {
+				File file = new File(link.get("URL").toString());
+
+				String body = extractHtml(file);
+
 				DBCursor rankingCursor = rankDB.find(new BasicDBObject("_id", link.get("URL").toString().trim()));
+				position = (ArrayList<Integer>) link.get("Position");
 				for (DBObject rank : rankingCursor) {
 
 					if (link.get("URL").toString().equals(rank.get("_id"))) {
 						double totalLink = (double) rank.get("PageRank") + (double) link.get("TFIDF");
 						word1mp.add(link.get("URL").toString());
 						tempAll.add(new Search(link.get("URL").toString(), (double) rank.get("PageRank"),
-								(double) link.get("TFIDF"), totalLink));
+								(double) link.get("TFIDF"), totalLink, position, body));
 					}
 
 				}
@@ -63,16 +92,24 @@ public class SearchFeatures {
 			}
 
 			DBCursor w2 = indexDB.find(new BasicDBObject("Term", word2));
+			ArrayList<Integer> position2 = new ArrayList<Integer>();
 
 			for (DBObject link : w2) {
+				File file = new File(link.get("URL").toString());
+
+				String body = extractHtml(file);
+
 				DBCursor rankingCursor = rankDB.find(new BasicDBObject("_id", link.get("URL").toString().trim()));
+
+				position2 = (ArrayList<Integer>) link.get("Position");
+
 				for (DBObject rank : rankingCursor) {
 
 					if (link.get("URL").toString().equals(rank.get("_id"))) {
 						double totalLink = (double) rank.get("PageRank") + (double) link.get("TFIDF");
 						word2mp.add(link.get("URL").toString());
 						tempAll.add(new Search(link.get("URL").toString(), (double) rank.get("PageRank"),
-								(double) link.get("TFIDF"), totalLink));
+								(double) link.get("TFIDF"), totalLink, position2, body));
 
 					}
 
@@ -94,16 +131,21 @@ public class SearchFeatures {
 		else {
 
 			DBCursor w1 = indexDB.find(new BasicDBObject("Term", word1));
+			ArrayList<Integer> position1 = new ArrayList<Integer>();
 
 			for (DBObject link : w1) {
+				File file = new File(link.get("URL").toString());
+
+				String body = extractHtml(file);
 				DBCursor rankingCursor = rankDB.find(new BasicDBObject("_id", link.get("URL").toString().trim()));
+				position1 = (ArrayList<Integer>) link.get("Position");
 				for (DBObject rank : rankingCursor) {
 
 					if (link.get("URL").toString().equals(rank.get("_id"))) {
 						double totalLink = (double) rank.get("PageRank") + (double) link.get("TFIDF");
 
 						search.add(new Search(link.get("URL").toString(), (double) rank.get("PageRank"),
-								(double) link.get("TFIDF"), totalLink));
+								(double) link.get("TFIDF"), totalLink, position1, body));
 					}
 
 				}
@@ -111,16 +153,21 @@ public class SearchFeatures {
 			}
 
 			DBCursor w2 = indexDB.find(new BasicDBObject("Term", word2));
+			ArrayList<Integer> position2 = new ArrayList<Integer>();
 
 			for (DBObject link : w2) {
+				File file = new File(link.get("URL").toString());
+
+				String body = extractHtml(file);
 				DBCursor rankingCursor = rankDB.find(new BasicDBObject("_id", link.get("URL").toString().trim()));
+				position2 = (ArrayList<Integer>) link.get("Position");
 				for (DBObject rank : rankingCursor) {
 
 					if (link.get("URL").toString().equals(rank.get("_id"))) {
 						double totalLink = (double) rank.get("PageRank") + (double) link.get("TFIDF");
 
 						search.add(new Search(link.get("URL").toString(), (double) rank.get("PageRank"),
-								(double) link.get("TFIDF"), totalLink));
+								(double) link.get("TFIDF"), totalLink, position2, body));
 					}
 
 				}
@@ -131,16 +178,17 @@ public class SearchFeatures {
 		return search;
 	}
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException, SAXException, TikaException {
 
 		System.out.println("[URL] [TFIDF] [Link Analysis] [Total]");
 
-		List<Search> search = boolDriver("sterling or township");
+		List<Search> search = boolDriver("sterling and township");
 
 		for (int i = 0; i < search.size(); i++) {
 
 			System.out.println(search.get(i).getURL() + " == " + search.get(i).getTfidf() + " == "
 					+ search.get(i).getLinkAnalysis() + " == " + search.get(i).getTotalRanking());
+			System.out.println(search.get(i).getPosition());
 		}
 		mongoClient.close();
 	}
